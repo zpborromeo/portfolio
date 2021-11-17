dd6aa7aa30313284b67359719ecf7a2026d6f9b3
zpborromeo <46539411+zpborromeo@users.noreply.github.com>
2021-11-17 11:36:16 -0600

    Update update_funding_cases.R

--- sm_core/dev/update_funding_cases.R
+++ sm_core/dev/update_funding_cases.R
@@ -7,24 +7,40 @@ funding_cases_table <- funding_cases_report %>%
   select(case_id, status, percent_funded, financial_account_balance) %>% 
   rename(id = case_id)
 
-updated_funding_cases <- funding_cases_table %>%
+updated_funding_cases_partial <- funding_cases_table %>%
   mutate(percent_funded = str_remove(percent_funded, coll('%')), 
          percent_funded = str_remove(percent_funded, coll(',')),
          financial_account_balance = str_remove(financial_account_balance, coll('$')),
          percent_funded = as.numeric(percent_funded)) %>%
   filter(status == "Account Opened") %>% 
-  mutate(status = if_else(percent_funded >= 75.00, "Fully Funded", status),
-         status = if_else(percent_funded >= 1.00 & percent_funded < 75.00, "Partially Funded", status),
-         status = if_else(financial_account_balance >= 1.00, "Partially Funded", status)) %>%
-  filter(status == "Fully Funded" | status == "Partially Funded") %>%
+  mutate(status = if_else(financial_account_balance >= 1.00, "Partially Funded", status)) %>%
+  filter( status == "Partially Funded") %>%
   select(-c(percent_funded, financial_account_balance))
 
-response <- sf_update(updated_funding_cases, "Bulk 2.0", object_name = "Case")
+updated_funding_cases_fully <- funding_cases_table %>%
+  mutate(percent_funded = str_remove(percent_funded, coll('%')), 
+         percent_funded = str_remove(percent_funded, coll(',')),
+         financial_account_balance = str_remove(financial_account_balance, coll('$')),
+         percent_funded = as.numeric(percent_funded)) %>%
+  filter(status == "Account Opened") %>% 
+  mutate(status = if_else(percent_funded >= 75.00, "Fully Funded", status)) %>% 
+  filter(status == "Fully Funded") %>%
+  select(-c(percent_funded, financial_account_balance))
 
-response <- updated_funding_cases %>% 
-  split(f = rep(1:ceiling(nrow(updated_funding_cases) / 10), each = 10)[1:nrow(updated_funding_cases)]) %>% 
+
+response_partiallyfunded <- updated_funding_cases_partial %>% 
+  split(f = rep(1:ceiling(nrow(updated_funding_cases_partial) / 10), each = 10)[1:nrow(updated_funding_cases_partial)]) %>% 
   map_dfr(~{
     
     sf_update(.x, object_name = "Case")
     
   })
+
+response_fullyfunded <- updated_funding_cases_fully %>% 
+  split(f = rep(1:ceiling(nrow(updated_funding_cases_fully) / 10), each = 10)[1:nrow(updated_funding_cases_fully)]) %>% 
+  map_dfr(~{
+    
+    sf_update(.x, object_name = "Case")
+    
+  })
+

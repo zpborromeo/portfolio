cd0f2c5338f70acfdb4ebedea626b37c10adf2ef
zpborromeo <46539411+zpborromeo@users.noreply.github.com>
2022-01-12 09:57:09 -0600

    Update security_master_audit.R

--- sm_core/dev/security_master_audit.R
+++ sm_core/dev/security_master_audit.R
@@ -2,9 +2,7 @@ source('utils.R')
 
 addepar_sm <- readxl::read_excel("Excel_Files/addepar-sm.xlsx") %>% 
   clean_names() %>% 
-  rename("sm_sec_asset_type" = sec_asset_type) 
-  # rename("addepar_cusip" = cusip) %>% 
-  # rename("addepar_ticker" = ticker)
+  rename("sm_sec_asset_type" = sec_asset_type)
 
 black_diamond_sm <- readxl::read_excel("Excel_Files/black-diamond-sm.xlsx") %>% 
   clean_names()
@@ -45,3 +43,40 @@ ticker_asset_type_missing <- ticker_match %>%
 
 ticker_issue_type_missing <- ticker_match %>% 
   filter(sm_issue_type == "-")
+
+ticker_upload_table <- ticker_match %>% 
+  select(ticker, entity_id, security, sm_class, sm_segment, sm_sector_segment, sm_issue_type, sm_sec_asset_type, issue_type,
+         class, segment, dual_segment, sec_asset_type, class_match, segment_match, sector_segment_match, issue_type_match, 
+         sec_asset_type_match, full_match) %>% 
+  mutate(new_class = if_else(class_match == TRUE, sm_class, class)) %>% 
+  mutate(new_class = if_else(is.na(class), sm_class, class)) %>% 
+  mutate(new_segment = if_else(segment_match == TRUE, sm_segment, segment)) %>%
+  mutate(new_segment = if_else(is.na(segment), sm_segment, segment)) %>% 
+  mutate(new_sector_segment = if_else(sector_segment_match == TRUE, sm_sector_segment, dual_segment)) %>%
+  mutate(new_sector_segment = if_else(is.na(dual_segment), sm_sector_segment, dual_segment)) %>% 
+  mutate(new_issue_type = if_else(issue_type_match == TRUE, sm_issue_type, issue_type)) %>%
+  mutate(new_issue_type = if_else(is.na(issue_type), sm_issue_type, issue_type)) %>% 
+  mutate(new_sec_asset_type = sec_asset_type) %>% 
+  select(ticker, entity_id, security, new_class, new_segment, new_sector_segment, new_issue_type, new_sec_asset_type)
+
+
+cusip_upload_table <- cusip_match %>% 
+  select(cusip, entity_id, security, sm_class, sm_segment, sm_sector_segment, sm_issue_type, sm_sec_asset_type, issue_type,
+         class, segment, dual_segment, sec_asset_type, class_match, segment_match, sector_segment_match, issue_type_match, 
+         sec_asset_type_match, full_match) %>% 
+  mutate(new_class = if_else(class_match == TRUE, sm_class, class)) %>% 
+  mutate(new_class = if_else(is.na(class), sm_class, class)) %>% 
+  mutate(new_segment = if_else(segment_match == TRUE, sm_segment, segment)) %>%
+  mutate(new_segment = if_else(is.na(segment), sm_segment, segment)) %>% 
+  mutate(new_sector_segment = if_else(sector_segment_match == TRUE, sm_sector_segment, dual_segment)) %>%
+  mutate(new_sector_segment = if_else(is.na(dual_segment), sm_sector_segment, dual_segment)) %>% 
+  mutate(new_issue_type = if_else(issue_type_match == TRUE, sm_issue_type, issue_type)) %>%
+  mutate(new_issue_type = if_else(is.na(issue_type), sm_issue_type, issue_type)) %>% 
+  mutate(new_sec_asset_type = sec_asset_type) %>% 
+  select(cusip, entity_id, security, new_class, new_segment, new_sector_segment, new_issue_type, new_sec_asset_type)
+
+save_document <- write.csv(ticker_upload_table, "C:/sm-core/sm_core/Excel_Files/ticker_upload.csv")
+
+save_document <- write.csv(cusip_upload_table, "C:/sm-core/sm_core/Excel_Files/cusip_upload.csv")
+
+

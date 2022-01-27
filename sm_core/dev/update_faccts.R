1fcd0068ea9f688f7af7b2cdc812b1ac56966bd0
zpborromeo <46539411+zpborromeo@users.noreply.github.com>
2022-01-27 11:22:06 -0600

    Update update_faccts.R

--- sm_core/dev/update_faccts.R
+++ sm_core/dev/update_faccts.R
@@ -1,6 +1,6 @@
 source('utils.R')
 
-account_update <- readxl::read_xlsx("Excel_Files/zenith-sw-accts.xlsx")
+account_update <- readxl::read_xlsx("Excel_Files/update-faccts.xlsx")
 
 response <- account_update %>% 
   split(f = rep(1:ceiling(nrow(account_update) / 10), each = 10)[1:nrow(account_update)]) %>% 

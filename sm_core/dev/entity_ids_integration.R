4feb062b538d2e3372b1b28d33688001d4b91d8d
zpborromeo <46539411+zpborromeo@users.noreply.github.com>
2022-01-31 08:51:03 -0600

    Update entity_ids_integration.R

--- sm_core/dev/entity_ids_integration.R
+++ sm_core/dev/entity_ids_integration.R
@@ -21,13 +21,15 @@ production_addepar_join <- sf_production_accounts %>%
   left_join(addepar_accounts, by = c("holding_account_number")) %>% 
   select(financial_account_name_production, financial_account_id, entity_id, holding_account_number) %>% 
   filter(!is.na(entity_id)) %>% 
-  filter(!is.na(holding_account_number))
+  filter(!is.na(holding_account_number)) %>% 
+  rename("Id" = financial_account_id)
 
 sandbox_addepar_join <- sf_sandbox_accounts %>% 
   left_join(addepar_accounts, by = c("holding_account_number")) %>% 
   select(financial_account_name_sandbox, financial_account_id, entity_id, holding_account_number) %>% 
   filter(!is.na(entity_id)) %>% 
-  filter(!is.na(holding_account_number))
+  filter(!is.na(holding_account_number)) %>% 
+  rename("Id" = financial_account_id)
 
 save_document <- write.csv(production_addepar_join, "Excel_Files/production_addepar_join.csv")
 save_document <- write.csv(sandbox_addepar_join, "Excel_Files/sandbox_addepar_join.csv")
